c25463f310a8f795acf28543f75ef73f57ef7198
dave morgan <dave@dmorg.org>
2023-07-24 18:14:39 -0600

    styling

--- src/routes/(app)/math/ALevelPure/11Integration/+page.svelte
+++ src/routes/(app)/math/ALevelPure/11Integration/+page.svelte
@@ -1,10 +1,11 @@
 <script>
   import Header from "$lib/components/Header.svelte";
+  import { ki } from "$lib/utilities";
+
   const topMatter = {
     theme: "math",
-    section: ["a-level pure math (year 1)", true, "/math/ALevelPure"], // to help with navigation and color styling
-    // subsect: ["Year 1", false],
-    subsubsect: ["11 Integration", false], // statics, strength, fluids
+    headerLeft: ["A-Level Pure Maths (Year 1)", true, "/math/ALevelPure"], // to help with navigation and color styling
+    headerRight: ["10 Differentiation", false], // statics, strength, fluids
   };
   let tm = topMatter;
 </script>
@@ -12,5 +13,38 @@
 <div class="container {tm.theme}">
   <Header {topMatter} />
 
-  <h1>coming soon</h1>
+  <main class="linknav">
+    <ul>
+      <li>
+        <a href="/"><h5>Home Page</h5></a>
+      </li>
+      <li>
+        <h4>11 Integration</h4>
+
+        <ul>
+          <li>
+            <a href="/math/ALevelPure/10Differentiation/101Gradient">
+              <h5><s>1 Integration</s></h5>
+            </a>
+
+            <a href="/math/ALevelPure/10Differentiation/102FromFirstPrinciples">
+              <h5><s>2 Integration of {@html `${ki(`\\bm{ x^n}`)}`}</s></h5>
+            </a>
+
+            <a href="/math/ALevelPure/10Differentiation/103PowersOfX">
+              <h5><s>3 Integration of Functions</s></h5>
+            </a>
+
+            <a href="/math/ALevelPure/10Differentiation/103PowersOfX">
+              <h5><s>4 Finding the Equation of Curves</s></h5>
+            </a>
+
+            <a href="/math/ALevelPure/10Differentiation/103PowersOfX">
+              <h5><s>5 Evaluation of Definite Integrals</s></h5>
+            </a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </main>
 </div>
