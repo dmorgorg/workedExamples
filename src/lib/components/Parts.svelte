cad43a47ca0f7280ef3f1a82b6a6286ac1696b20
dave morgan <dave@dmorg.org>
2023-07-26 22:18:39 -0600

    styling

--- src/lib/components/Parts.svelte
+++ src/lib/components/Parts.svelte
@@ -7,7 +7,7 @@
 <!-- problem[1] is the main problem statement,
       problem[2] is the array of solution and answer parts
       and problem[3] is the main problem answer  -->
-<section>
+<section class="problem-parts">
   <!-- don't need problem statement and show-hide button if only one part to the proof  -->
   {#if problem[2].length <= 1}
     <div class="single">{@html problem[2][0][0]}</div>
@@ -24,11 +24,12 @@
 
 <style>
   section {
-    color: black;
     counter-reset: part-counter;
-    /* margin: 0.5em; */
   }
   /* .single {
       margin: 1em 2em;
     } */
+  .problem-parts {
+    /* background: yellow; */
+  }
 </style>
